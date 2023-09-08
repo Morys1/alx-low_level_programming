03688cc1f8b8a407d4a867e145e7dd500c8eaf1d
Morys1 <savanemory99@gmail.com>
2023-09-08 01:51:09 -0700

    Write a C program that prints exactly a sentence

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,15 @@
+#include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-puts("Programming is like building a multilingual puzzle");
-return (0);
+  char* txt = "Programming is like building a multilingual puzzle";
+
+  puts(txt);
+
+  return (0);
 }
